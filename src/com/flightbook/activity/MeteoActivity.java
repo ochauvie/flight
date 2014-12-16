341c63528ad916ccd2c5c51edc2dc2f923d53591
ochauvie <o.chauvie@novedia-solutions.com>
2014-12-16 15:43:59 +0100

    Meteo
    Metar by Cunimb and AADS

--- src/com/flightbook/activity/MeteoActivity.java
+++ src/com/flightbook/activity/MeteoActivity.java
@@ -14,13 +14,20 @@ import android.widget.ImageButton;
 
 public class MeteoActivity extends Activity {
 	
-	private ImageButton mButton;
+	private ImageButton mButtonCunimb;
+    private ImageButton mButtonADDS;
 	private WebView webView;
 	private EditText editTextOaci;
 	private ProgressDialog mProgress;
 
-	
-	/** Called when the activity is first created. */
+    private String meteoCunimbUrlStart = "http://cunimb.net/decodemet.php?station=";
+    private String meteoCunimbUrlEnd = "";
+
+    private String meteoADDSUrlStart = "http://www.aviationweather.gov/adds/metars/?station_ids=";
+    private String meteoADDSUrlEnd = "&std_trans=translated&chk_metars=on&hoursStr=most+recent+only&submitmet=Submit";
+
+
+    /** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -34,7 +41,7 @@ public class MeteoActivity extends Activity {
 
         // Add a WebViewClient for WebView, which actually handles loading data from web
 		webView = (WebView) findViewById(R.id.webview);
-		webView.loadUrl("http://cunimb.net/decodemet.php?station=" + editTextOaci.getText().toString());
+		webView.loadUrl(meteoADDSUrlStart + editTextOaci.getText().toString() + meteoADDSUrlEnd);
 		
 		webView.setWebViewClient(new WebViewClient() {
             // load url
@@ -51,16 +58,26 @@ public class MeteoActivity extends Activity {
             }
         });
 
-		mButton = (ImageButton) findViewById(R.id.btnLaunch);
-		mButton.setOnClickListener(new View.OnClickListener() {
+		mButtonCunimb = (ImageButton) findViewById(R.id.btnLaunchCunimb);
+		mButtonCunimb.setOnClickListener(new View.OnClickListener() {
         	public void onClick(View v) {
         		String oaci = editTextOaci.getText().toString();
         		if (oaci!=null && !"".equals(oaci)) {
-        			String url = "http://cunimb.net/decodemet.php?station=" + oaci.toUpperCase();
-        			//String url = "http://www.aviationweather.gov/adds/metars/?station_ids=" + oaci.toUpperCase() + "&std_trans=translated&chk_metars=on&hoursStr=most+recent+only&submitmet=Submit";
+        			String url = meteoCunimbUrlStart + oaci.toUpperCase() + meteoCunimbUrlEnd;
         			webView.loadUrl(url);
         		}
         	}
+        });
+
+        mButtonADDS = (ImageButton) findViewById(R.id.btnLaunchADDS);
+        mButtonADDS.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String oaci = editTextOaci.getText().toString();
+                if (oaci!=null && !"".equals(oaci)) {
+                    String url = meteoADDSUrlStart + oaci.toUpperCase() + meteoADDSUrlEnd;
+                    webView.loadUrl(url);
+                }
+            }
         });
 			
 	}
